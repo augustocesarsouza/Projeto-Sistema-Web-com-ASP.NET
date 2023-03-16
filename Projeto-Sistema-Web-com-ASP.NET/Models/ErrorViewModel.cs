adccb85003fdc65a48ec4c467f0f29812b5d68e8
augustocesarsouza <augustocesarsantana90@gmail.com>
2023-03-16 06:22:29 -0400

    Project create

---
+++ Projeto-Sistema-Web-com-ASP.NET/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Projeto_Sistema_Web_com_ASP.NET.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
