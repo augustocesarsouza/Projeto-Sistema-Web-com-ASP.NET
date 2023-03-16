748dddc1fc68dac78a2c10c7c3a4f96151d764f6
augustocesarsouza <augustocesarsantana90@gmail.com>
2023-03-16 17:51:17 -0400

    Update seller and custom service exception

---
+++ Projeto-Sistema-Web-com-ASP.NET/Services/Excepetion/DbConcurrencyException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projeto_Sistema_Web_com_ASP.NET.Services.Excepetion
+{
+    public class DbConcurrencyException : ApplicationException
+    {
+        public DbConcurrencyException(string message) : base(message)
+        {
+        }
+    }
+}
---
+++ Projeto-Sistema-Web-com-ASP.NET/Services/Excepetion/NotFoundException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Projeto_Sistema_Web_com_ASP.NET.Services.Excepetion
+{
+    public class NotFoundException : ApplicationException
+    {
+        public NotFoundException(string message) : base(message)
+        {
+        }
+    }
+}
