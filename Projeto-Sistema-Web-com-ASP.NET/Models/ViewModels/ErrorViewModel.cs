0d26132d88211142ad3c58b2b2a19446ac2a9bfd
augustocesarsouza <augustocesarsantana90@gmail.com>
2023-03-16 08:49:26 -0400

    Deleting Department view and controller

---
+++ Projeto-Sistema-Web-com-ASP.NET/Models/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Projeto_Sistema_Web_com_ASP.NET.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
