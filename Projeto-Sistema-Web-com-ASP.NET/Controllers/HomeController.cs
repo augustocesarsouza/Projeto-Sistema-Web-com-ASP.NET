1536f2ca07c22095e53d5e7cacb25a01c7a83dba
augustocesarsouza <augustocesarsantana90@gmail.com>
2023-03-16 08:06:33 -0400

    First controller and Razor pages tests

--- Projeto-Sistema-Web-com-ASP.NET/Controllers/HomeController.cs
+++ Projeto-Sistema-Web-com-ASP.NET/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace Projeto_Sistema_Web_com_ASP.NET.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Salles Web MVC App from C# Course";
+            ViewData["Professor"] = "Nelio Alves";
 
             return View();
         }
