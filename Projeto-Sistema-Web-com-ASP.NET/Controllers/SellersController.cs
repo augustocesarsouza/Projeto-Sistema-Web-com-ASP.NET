777dec108ee62b7480e2f80ddf4d4ab727379da8
augustocesarsouza <augustocesarsantana90@gmail.com>
2023-03-16 17:23:49 -0400

    Delete seller

--- Projeto-Sistema-Web-com-ASP.NET/Controllers/SellersController.cs
+++ Projeto-Sistema-Web-com-ASP.NET/Controllers/SellersController.cs
@@ -40,5 +40,29 @@ namespace Projeto_Sistema_Web_com_ASP.NET.Controllers
             _sellerService.Insert(seller);
             return RedirectToAction(nameof(Index));
         }
+
+        public IActionResult Delete(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = _sellerService.FindById(id.Value);
+            if(obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            _sellerService.Remove(id);
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
