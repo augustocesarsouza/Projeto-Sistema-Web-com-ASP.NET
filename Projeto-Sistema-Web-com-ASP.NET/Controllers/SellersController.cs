77bf348dda54c52e8b9c0b45636f93eb71980947
augustocesarsouza <augustocesarsantana90@gmail.com>
2023-03-16 17:32:49 -0400

    Seller details and eager loading

--- Projeto-Sistema-Web-com-ASP.NET/Controllers/SellersController.cs
+++ Projeto-Sistema-Web-com-ASP.NET/Controllers/SellersController.cs
@@ -64,5 +64,21 @@ namespace Projeto_Sistema_Web_com_ASP.NET.Controllers
             _sellerService.Remove(id);
             return RedirectToAction(nameof(Index));
         }
+
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = _sellerService.FindById(id.Value);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
     }
 }
